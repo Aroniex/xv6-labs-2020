53b691ddabc2391bb3ddb3e3450ae44b6d503e6d
Aroniex <1257726859@qq.com>
2023-12-20 20:21:58 +0800

    finish trap

--- kernel/proc.h
+++ kernel/proc.h
@@ -93,6 +93,10 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  int interval;
+  uint64 handler;
+  int passedticks;
+  struct trapframe* trapframecopy;      // the copy of trapframe - lab4-3
 
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
